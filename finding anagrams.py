71924debe21636699dd4859eabea1aa720e4eb94
ESTHER ISRAEL OKON <105334293+Smartbabbe@users.noreply.github.com>
2022-05-29 00:02:25 +0100

    anagrams

---
+++ finding anagrams.py
@@ -0,0 +1,19 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+from pickletools import string1
+import string
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+  if (sorted(word)) == sorted(anagram(anagram)):
+  
+    return True
+  else:
+      return True
+  
+print(find_anagram("knee","keen"))  
